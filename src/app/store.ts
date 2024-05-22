4db50e5835ee94e9a0d3af2f0789eb2156d5ceac
Darnose <andrey.yarovoy.2013@gmail.com>
2024-05-22 10:57:22 +0300

    created userApi

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,15 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { configureStore } from "@reduxjs/toolkit"
+import { api } from "./services/api"
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
 })
 
-// Infer the type of `store`
 export type AppStore = typeof store
 export type RootState = ReturnType<typeof store.getState>
-// Infer the `AppDispatch` type from the store itself
 export type AppDispatch = AppStore["dispatch"]
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
