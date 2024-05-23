633c88d0a0232207878d43a87bb48d2c11d09f05
Darnose <andrey.yarovoy.2013@gmail.com>
2024-05-23 22:20:24 +0300

    created commentsApi

---
+++ src/app/services/commentsApi.ts
@@ -0,0 +1,27 @@
+import { api } from "./api"
+import type { Comment } from "../types"
+
+export const commentsApi = api.injectEndpoints({
+  endpoints: builder => ({
+    createComment: builder.mutation<Comment, Partial<Comment>>({
+      query: newComment => ({
+        url: "/comments",
+        method: "POST",
+        body: newComment,
+      }),
+    }),
+    deleteComment: builder.mutation<void, string>({
+      query: commentId => ({
+        url: `/comments/${commentId}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+})
+
+export const { useCreateCommentMutation, useDeleteCommentMutation } =
+  commentsApi
+
+export const {
+  endpoints: { createComment, deleteComment },
+} = commentsApi
