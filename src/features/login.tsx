2b78c5b440797f44ce46103e0f626b6b7ce156a8
Darnose <andrey.yarovoy.2013@gmail.com>
2024-05-29 17:48:08 +0300

    created Input component + auth page

---
+++ src/features/login.tsx
@@ -0,0 +1,74 @@
+import { Input } from "../components/input"
+import { useForm } from "react-hook-form"
+import { Button, Link } from "@nextui-org/react"
+import { useLazyCurrentQuery, useLoginMutation } from "../app/services/userApi"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
+type Login = {
+  email: string
+  password: string
+}
+
+type Props = {
+  setSelected: (value: string) => void
+}
+
+export const Login = ({ setSelected }: Props) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Login>({
+    mode: "onChange",
+    reValidateMode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const [login, { isLoading }] = useLoginMutation
+  const navigate = useNavigate()
+  const [error, setError] = useState("")
+  const [triggerCurrentQuery] = useLazyCurrentQuery()
+
+  const onSubmit = async (data: Login) => {
+    try {
+      await login(data).unwrap()
+    } catch (error) {}
+  }
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        control={control}
+        name="email"
+        label="Email"
+        type="email"
+        required="Обязательное поле"
+      />
+      <Input
+        control={control}
+        name="password"
+        label="Пароль"
+        type="password"
+        required="Обязательное поле"
+      />
+      <p className="text-center text-small">
+        Нет аккаутна?{" "}
+        <Link
+          size="sm"
+          className="cursor-pointer"
+          onPress={() => setSelected("sign-up")}
+        >
+          Зарегистрируйтесь
+        </Link>
+      </p>
+      <div className="flex gap-2 justify-end">
+        <Button fullWidth color="primary" type="submit" isLoading={isLoading}>
+          Войти
+        </Button>
+      </div>
+    </form>
+  )
+}
