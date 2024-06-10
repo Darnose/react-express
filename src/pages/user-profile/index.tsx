8388d082dd77905359a9be2845ad5af1ea7f9b9e
Darnose <andrey.yarovoy.2013@gmail.com>
2024-06-10 16:33:16 +0300

    user profile p.1

--- src/pages/user-profile/index.tsx
+++ src/pages/user-profile/index.tsx
@@ -1,5 +1,80 @@
-import React from "react"
+import { Button, Card, Image, useDisclosure } from "@nextui-org/react"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { resetUser, selectCurrent } from "../../features/user/userSlice"
+import {
+  useGetUserByIdQuery,
+  useLazyCurrentQuery,
+  useLazyGetUserByIdQuery,
+} from "../../app/services/userApi"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import {
+  useFollowUserMutation,
+  useUnFollowUserMutation,
+} from "../../app/services/followApi"
+import { GoBack } from "../../components/go-back"
+import { BASE_URL } from "../../constants"
+import {
+  MdOutlinePersonAddAlt1,
+  MdOutlinePersonAddDisabled,
+} from "react-icons/md"
+import { CiEdit } from "react-icons/ci"
 
 export const UserProfile = () => {
-  return <div>UserProfile</div>
+  const { id } = useParams<{ id: string }>()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const currentUser = useAppSelector(selectCurrent)
+  const { data } = useGetUserByIdQuery(id ?? "")
+  const [followUser] = useFollowUserMutation()
+  const [unFollowUser] = useUnFollowUserMutation()
+  const [triggerGetUserById] = useLazyGetUserByIdQuery()
+  const [triggerCurrentQuery] = useLazyCurrentQuery()
+
+  const dispatch = useAppDispatch()
+
+  useEffect(
+    () => () => {
+      dispatch(resetUser())
+    },
+    [],
+  )
+
+  return (
+    <>
+      <GoBack />
+      <div className="flex items-center gap-4">
+        <Card className="flex flex-col items-center text-center space-y-4 p-5 flex-2">
+          <Image
+            src={`${BASE_URL}${data?.avatarUrl}`}
+            alt={data?.name}
+            width={200}
+            height={200}
+            className="border-4 border-white"
+          />
+          <div className="flex flex-col text-2xl font-bold gap-4 item-center">
+            {data?.name}
+            {currentUser?.id !== id ? (
+              <Button
+                color={data?.isFollowing ? "default" : "primary"}
+                variant="flat"
+                className="gap-2"
+                endContent={
+                  data?.isFollowing ? (
+                    <MdOutlinePersonAddDisabled />
+                  ) : (
+                    <MdOutlinePersonAddAlt1 />
+                  )
+                }
+              >
+                {data?.isFollowing ? "Отписаться" : "Подписаться"}
+              </Button>
+            ) : (
+              <Button endContent={<CiEdit />}>Редактировать</Button>
+            )}
+          </div>
+        </Card>
+        <Card className="flex flex-col space-y-4 p-5 flex-1"></Card>
+      </div>
+    </>
+  )
 }
