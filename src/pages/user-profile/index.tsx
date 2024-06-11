c9f45cba8cca866ad9ae01f039717b6de2ef6246
Darnose <andrey.yarovoy.2013@gmail.com>
2024-06-11 11:14:35 +0300

    edit profile p.1

--- src/pages/user-profile/index.tsx
+++ src/pages/user-profile/index.tsx
@@ -22,6 +22,7 @@ import { CiEdit } from "react-icons/ci"
 import { ProfileInfo } from "../../components/profile-info"
 import { formatToClientDate } from "../../utils/format-to-client-date"
 import { CountInfo } from "../../components/count-info"
+import { EditProfile } from "../../components/edit-profile"
 
 export const UserProfile = () => {
   const { id } = useParams<{ id: string }>()
@@ -92,7 +93,9 @@ export const UserProfile = () => {
                 {data.isFollowing ? "Отписаться" : "Подписаться"}
               </Button>
             ) : (
-              <Button endContent={<CiEdit />}>Редактировать</Button>
+              <Button endContent={<CiEdit />} onClick={() => onOpen()}>
+                Редактировать
+              </Button>
             )}
           </div>
         </Card>
@@ -110,6 +113,7 @@ export const UserProfile = () => {
             <CountInfo count={data.following.length} title="Подписки" />
           </div>
         </Card>
+        <EditProfile isOpen={isOpen} onClose={onClose} user={data} />
       </div>
     </>
   )
